700f544f549dea1ac4bde0ec890afb048fb87604
Miguel Ángel <20430676+supermavster@users.noreply.github.com>
2023-12-12 15:59:19 -0500

    Update create_completion.py

--- create_completion.py
+++ create_completion.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-
-import openai
+from openai import OpenAI
 import sys
 import os
 import configparser
@@ -9,17 +8,9 @@ import configparser
 CONFIG_DIR = os.getenv('XDG_CONFIG_HOME', os.path.expanduser('~/.config'))
 API_KEYS_LOCATION = os.path.join(CONFIG_DIR, 'openaiapirc')
 
-# Read the organization_id and secret_key from the ini file ~/.config/openaiapirc
-# The format is:
-# [openai]
-# organization_id=<your organization ID>
-# secret_key=<your secret key>
-
-# If you don't see your organization ID in the file you can get it from the
-# OpenAI web site: https://openai.com/organizations
 def create_template_ini_file():
     """
-    If the ini file does not exist create it and add the organization_id and
+    If the ini file does not exist, create it and add the organization_id and
     secret_key
     """
     if not os.path.isfile(API_KEYS_LOCATION):
@@ -29,34 +20,23 @@ def create_template_ini_file():
             f.write('secret_key=\n')
             f.write('model=gpt-3.5-turbo-0613\n')
 
-        print('OpenAI API config file created at {}'.format(API_KEYS_LOCATION))
+        print(f'OpenAI API config file created at {API_KEYS_LOCATION}')
         print('Please edit it and add your organization ID and secret key')
         print('If you do not yet have an organization ID and secret key, you\n'
                'need to register for OpenAI Codex: \n'
-                'https://openai.com/blog/openai-codex/')
+               'https://openai.com/blog/openai-codex/')
         sys.exit(1)
 
+# Check if file at API_KEYS_LOCATION exists
+create_template_ini_file()
+config = configparser.ConfigParser()
+config.read(API_KEYS_LOCATION)
 
-def initialize_openai_api():
-    """
-    Initialize the OpenAI API
-    """
-    # Check if file at API_KEYS_LOCATION exists
-    create_template_ini_file()
-    config = configparser.ConfigParser()
-    config.read(API_KEYS_LOCATION)
+client = OpenAI(
+  api_key=config['openai']['secret_key'].strip('"').strip("'")
+)
 
-    openai.organization_id = config['openai']['organization_id'].strip('"').strip("'")
-    openai.api_key = config['openai']['secret_key'].strip('"').strip("'")
-
-    if 'model' in config['openai']:
-        model = config['openai']['model'].strip('"').strip("'")
-    else:
-        model = 'gpt-3.5-turbo'
-
-    return model
-
-model = initialize_openai_api()
+model = config['openai'].get('model', 'gpt-3.5-turbo')
 
 cursor_position_char = int(sys.argv[1])
 
@@ -65,17 +45,15 @@ buffer = sys.stdin.read()
 prompt_prefix = '#!/bin/zsh\n\n' + buffer[:cursor_position_char]
 prompt_suffix = buffer[cursor_position_char:]
 full_command = prompt_prefix + prompt_suffix
-response = openai.ChatCompletion.create(model=model, messages=[
+response = client.chat.completions.create(model=model, messages=[
     {
-        "role":'system',
+        "role": 'system',
         "content": "You are a zsh shell expert, please help me complete the following command, you should only output the completed command, no need to include any other explanation",
     },
     {
-        "role":'user',
+        "role": 'user',
         "content": full_command,
     }
 ])
-completed_command = response['choices'][0]['message']['content']
-
+completed_command = response.choices[0].message.content
 sys.stdout.write(f"\n{completed_command.replace(prompt_prefix, '', 1)}")
-
